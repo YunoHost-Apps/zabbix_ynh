c406e992014c8c92d289ce79675c42bf54bb721d
Mickael-Martin <framagit@librement-votre.fr>
2019-07-05 13:41:54 +0200

    add file usr_share_zabbix_conf_zabbix.conf.php

---
+++ sources/extra_files/app/usr_share_zabbix_conf_zabbix.conf.php
@@ -0,0 +1,18 @@
+// Zabbix GUI configuration file.
+global \$DB;
+
+$DB['TYPE']     = 'MYSQL';
+$DB['SERVER']   = 'localhost';
+$DB['PORT']     = '0';
+$DB['DATABASE'] = 'db_name';
+$DB['USER']     = 'db_user';
+$DB['PASSWORD'] = 'db_pwd';
+
+// Schema name. Used for IBM DB2 and PostgreSQL.
+$DB['SCHEMA'] = '';
+
+$ZBX_SERVER      = 'localhost';
+$ZBX_SERVER_PORT = '10051';
+$ZBX_SERVER_NAME = 'zabbix-server';
+
+$IMAGE_FORMAT_DEFAULT = IMAGE_FORMAT_PNG;
